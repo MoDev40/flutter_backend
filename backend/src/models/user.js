7e37cd06220386bee93a22cfef46638582394554
M40 <modev.404@gmail.com>
2024-12-31 18:57:12 +0300

    initial setup with authentication

---
+++ backend/src/models/user.js
@@ -0,0 +1,24 @@
+import { model, Schema } from "mongoose";
+
+const UserSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  name: {
+    type: String,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    required: true,
+    default: "User",
+  },
+});
+
+const User = model("User", UserSchema);
+export default User;

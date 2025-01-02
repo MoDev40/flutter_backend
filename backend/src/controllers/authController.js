379622fa50f3080f34713afd3e0a5ef05f8c6672
M40 <modev.404@gmail.com>
2025-01-03 01:51:32 +0300

    get current user

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -45,3 +45,16 @@ export async function loginUser(req, res) {
     res.status(500).json({ message: "Server Error" });
   }
 }
+
+export const user = async (req, res) => {
+  try {
+    const id = req.user;
+    const user = await User.findById(id);
+    user.password = undefined;
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({
+      message: "Error fetching user analysis Internal server error",
+    });
+  }
+};
